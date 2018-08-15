03ca40b62dbb0f6998303efbf32992292dbbdece
Carlos Henrique <carloshenry.wm@gmail.com>
2018-08-15 13:26:51 -0300

    Changed the name of the color variables

--- app/src/main/java/com/example/henry/forkit/MainActivity.kt
+++ app/src/main/java/com/example/henry/forkit/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.henry.forkit
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
